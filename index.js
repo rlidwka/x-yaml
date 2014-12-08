1eaf39be612d1bba45870780acf0a7109ae49dbc
Alex Kocharin <alex@kocharin.ru>
2014-12-08 03:26:10 +0300

    correct yaml exception

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ Object.keys(yaml).forEach(function(k) {
 })
 
 // https://github.com/nodeca/js-yaml/pull/152
-exports.stringify = yaml.safeDump
-exports.parse = yaml.safeLoad
+exports.stringify = function stringify(obj, options) {
+  return yaml.safeDump(obj, options)
+}
+
+exports.parse = function parse(obj, options) {
+  try {
+    return yaml.safeLoad(obj, options)
+  } catch(orig) {
+    if (orig.name === 'YAMLException') {
+      var err = SyntaxError(orig.message)
+      err.reason = orig.reason
+      err.row    = orig.mark.line
+      err.column = orig.mark.column
+      err.mark   = orig.mark
+    }
+  }
+}
